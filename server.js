6e2e63b085ad882da9d904bd8244d5eaa895da61
Om Doiphode <omdoiphode161@gmail.com>
2025-03-24 09:58:53 +0530

    Added cors

--- server.js
+++ server.js
@@ -7,12 +7,13 @@ import cors from 'cors';
 const PORT = process.env.PORT || 8000;
 const app = express();
 app.use(express.json());
-app.use(cors({
-    origin: "https://hawkeye-gpt.netlify.app", // Allow your Netlify frontend
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type"],
-    credentials: true
-}));
+app.use(cors());
+// app.use(cors({
+//     origin: "https://hawkeye-gpt.netlify.app", // Allow your Netlify frontend
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     allowedHeaders: ["Content-Type"],
+//     credentials: true
+// }));
 
 mongoose.connect(process.env.MONGO_URI,{
     useNewUrlParser: true,
